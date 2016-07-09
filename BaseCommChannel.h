146ddf6ef895ada72ecf5595ac9e31c5fa2441f0
phoenixikkifullstack <phoenixikkifullstack@foxmail.com>
2016-07-09 10:13:27 +0800

    cm:(BaseCommChannel.h)First commit

---
+++ BaseCommChannel.h
@@ -0,0 +1,21 @@
+#ifndef __INCLUDE_EMIC_BASE_COMM_CHANNEL_H__
+#define __INCLUDE_EMIC_BASE_COMM_CHANNEL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <signal.h>
+#include <pthread.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include 
+#endif
